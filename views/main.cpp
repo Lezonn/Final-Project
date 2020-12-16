cd8262acb0d56e7efb9b118a3ebfabd23b7b9242
Lezonnn <73393716+Lezonnn@users.noreply.github.com>
2020-12-16 23:23:51 +0700

    Create main.cpp

---
+++ views/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../controllers/utils.h"
+#include "../controllers/controllers.h"
+
+int main() {
+    profileScreen();
+    return 0;
+}
+
